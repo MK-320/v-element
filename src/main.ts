f6ccea47871bde7c684ccca031021ebc8f0dcbaf
drgeek <321123809@qq.com>
2025-03-26 10:24:12 +0800

    both push

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './styles/index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 
