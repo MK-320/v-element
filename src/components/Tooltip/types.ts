890229faf428a8a6dd57cb7de6cade9c830a92e3
drgeek <321123809@qq.com>
2025-03-28 22:42:28 +0800

    完成Tooltip组件开发

--- src/components/Tooltip/types.ts
+++ src/components/Tooltip/types.ts
@@ -1,11 +1,22 @@
-import type { Placement } from '@popperjs/core'
+import type { Placement, Options } from '@popperjs/core'
 export interface TooltipProps {
   content?: string
   trigger?: 'hover' | 'click'
   placement: Placement
+  manual?: boolean
+  popperOptions?: Partial<Options> //这个options里面包含了placement，所以要是传入options的优先级要比placement更高，会覆盖掉
+  // option是Popperjs的配置项，默认里面的每个参数都是必选的，
+  // 使用ts提供的Partial类型，可以将每个参数都变成可选的
+  transition?: string
+  openDelay?: number
+  closeDelay?: number
 }
 
 export interface TooltipEmits {
   // eslint-disable-next-line no-unused-vars
   (e: 'visible-change', value: boolean): void
+}
+export interface TooltipInstance {
+  hide: () => void
+  show: () => void
 }
